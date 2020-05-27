5cb678239a952d140d91b8be7ab5e7c0b8659823
Jagadeesha Hegde <65488721+Jagadeesha-Hegde@users.noreply.github.com>
2020-05-27 06:51:46 +0530

    Create Weblckr.py

---
+++ 26 May 2020/Weblckr.py
@@ -0,0 +1,7 @@
+import time
+host_path = "c:r\\windows\system32\drivers\etc\hosts"
+redirect = "127.0.0.1"
+website_list = ["www.facebook.com","facebook.com","www.dub119.mail.live.com","dub119.mail.live.com"]
+while True:
+        print(1)
+        time.sleep(5)

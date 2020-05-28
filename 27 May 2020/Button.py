5d56107f809305e6b06f3652ed83eb5784ffebcf
Jagadeesha Hegde <65488721+Jagadeesha-Hegde@users.noreply.github.com>
2020-05-28 06:56:28 +0530

    Create Button.py

---
+++ 27 May 2020/Button.py
@@ -0,0 +1,9 @@
+from tkinter import *
+window = Tk()
+window.title("Welcome")
+window.geometry('350x200')
+lbl = Label(window, text="Hello")
+lbl.grid(column=0, row=0)
+btn = Button(window, text="Click here")
+btn.grid(column=1, row=0)
+window.mainloop()

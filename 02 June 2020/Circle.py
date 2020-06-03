a33fa6c05154606af1e6c4990adfa6af934aadcb
Jagadeesha Hegde <65488721+Jagadeesha-Hegde@users.noreply.github.com>
2020-06-03 09:27:29 +0530

    Create Circle.py

---
+++ 02 June 2020/Circle.py
@@ -0,0 +1,37 @@
+#importing Bokeh
+
+from bokeh.plotting import figure
+
+from bokeh.io import output_file, show
+
+ 
+
+#prepare some data
+
+x=[3,7.5,10]
+
+y=[3,6,9]
+
+ 
+
+#prepare the output file
+
+output_file("Line.html")
+
+ 
+
+#create a figure object
+
+f=figure()
+
+ 
+
+#create line plot
+
+f.circle(x,y)
+
+ 
+
+#write the plot in the figure object
+
+show(f)
